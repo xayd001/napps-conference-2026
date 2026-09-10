6ca414a08d566e72e0339208f982baf51f0d1561
xayd001 <zaydibnmukhtar01@gmail.com>
2026-09-10 11:04:27 +0100

    Update super admin configuration and firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging";
 
-// Replace with your Firebase Project Configuration keys
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "napps-conference-2026.firebaseapp.com",
-  projectId: "napps-conference-2026",
-  storageBucket: "napps-conference-2026.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDvfBHuK3rmKby1mdpG72KfD4OBYtb6BQ0",
+  authDomain: "napps-conference-2026-22771.firebaseapp.com",
+  projectId: "napps-conference-2026-22771",
+  storageBucket: "napps-conference-2026-22771.firebasestorage.app",
+  messagingSenderId: "641590693182",
+  appId: "1:641590693182:web:68cd20aab1929f1a43306f"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const messaging = getMessaging(app);
+export default app;
